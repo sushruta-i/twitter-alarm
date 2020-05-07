015cd40ba74c710180948b1eb7fc0b9d9db0782a
sushruta-i <62663406+sushruta-i@users.noreply.github.com>
2020-05-07 09:01:27 -0400

    Update and rename mcatwait.py to tweetwait.py

--- tweetwait.py
+++ tweetwait.py
@@ -1,4 +1,4 @@
-# Runs alarm.mp3 when the aamc_mcat twitter account updates
+# Runs alarm.mp3 when the chosen twitter account updates
 
 import tweepy
 import keys
@@ -11,19 +11,19 @@ auth=tweepy.OAuthHandler(keys.consumer_key, keys. consumer_secret)
 auth.set_access_token(keys.access_token, keys.access_token_secret)
 
 api=tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
-aamc = api.get_user('aamc_mcat')
+name = input('Enter twitter account: ')
+account = api.get_user(name)
 
 initial=aamc.status.text
 print(initial)
 
 
-def mcat_wait():
+def tweet_wait():
     interval = 1.
     next_t = time.time()
     while(datetime.now().hour < 12):
-        if aamc.status.text != initial:
+        if account.status.text != initial:
             os.system('alarm.mp3')
-            print('Sign up fool')
             break
         else:
             next_t += interval
