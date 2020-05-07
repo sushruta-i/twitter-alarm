bde24e96361f44958e4645eeecb7335629915e4e
sushruta-i <62663406+sushruta-i@users.noreply.github.com>
2020-05-07 09:15:57 -0400

    Update tweetwait.py

--- tweetwait.py
+++ tweetwait.py
@@ -7,7 +7,7 @@ import time
 import os
 
 
-auth=tweepy.OAuthHandler(keys.consumer_key, keys. consumer_secret)
+auth=tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
 auth.set_access_token(keys.access_token, keys.access_token_secret)
 
 api=tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
